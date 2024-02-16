8adb60cbb4778e0f15135f15e831a217ecee4723
djhkbw <djhkbw@umsystem.edu>
2024-02-16 17:15:57 -0600

    Added protected fields in MediaProduct

---
+++ src/test/MediaProduct.java
@@ -0,0 +1,19 @@
+package test;
+
+public class MediaProduct {
+	
+	private String title;
+	private double price;
+	private int year;
+	
+	private enum Genre {
+		ROCK,
+		POP,
+		JAZZ,
+		CLASSICAL,
+		HIP_HOP,
+		ELECTRONIC,
+		CHILDREN
+	}
+	
+}
