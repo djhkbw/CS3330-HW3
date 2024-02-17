dcc0e236d94536322f0b51337916935572494999
sblick19 <sophiecblick@gmail.com>
2024-02-16 20:10:39 -0600

    rewrite with copy constructor and genre

--- src/test/CDRecordProduct.java
+++ src/test/CDRecordProduct.java
@@ -2,11 +2,12 @@ package test;
 
 public class CDRecordProduct extends MediaProduct {
 	
-	public CDRecordProduct(String title, double price, int year) {
-		super(title, price, year);
-	}
-	
-	public CDRecordProduct(CDRecordProduct CD) {
-		super(CD.title, CD.price, CD.year);
-	}
+	   public CDRecordProduct(String title, double price, int year, Genre genre) {
+	        super(title, price, year, genre);
+	    }
+
+	    // Copy constructor
+	    public CDRecordProduct(CDRecordProduct other) {
+	        super(other);
+	    }
 }
