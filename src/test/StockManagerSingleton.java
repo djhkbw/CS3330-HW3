5f7bd96ab1ad5607526d1f092cf0cf01a7f37611
Antedbell20 <78629236+Antedbell20@users.noreply.github.com>
2024-02-16 21:41:38 -0600

    added the update item price

--- src/test/StockManagerSingleton.java
+++ src/test/StockManagerSingleton.java
@@ -3,7 +3,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-import java.util.ArrayList;
 
 import test.MediaProduct.Genre;
 
@@ -49,6 +48,39 @@ public class StockManagerSingleton
 	        return false; // Return false if an exception occurs (file not found, etc.)
 	    }
 	}
+	    //updates the price of a media product
+    public boolean updateItemPrice(MediaProduct product, double newPrice)
+    {
+        try( BufferedReader reader = new BufferedReader(new FileReader(inventoryFilePath)))
+        {
+            String line;
+            while((line = reader.readLine()) != null)
+            {
+                String[] parts = line.split(",");
+                if(parts.length == 4)
+                {
+                    String title = parts[0].trim();
+                    double price = Double.parseDouble(parts[1].trim());
+                    int year = Integer.parseInt(parts[2].trim());
+                    Genre genre = Genre.valueOf(parts[3].trim());
+                    MediaProduct product1 = new MediaProduct(title, price, year, genre);
+                    if(product1.equals(product))
+                    {
+                        product1.setPrice(newPrice);
+                        
+                    }
+                }
+                
+            }
+            return true;
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
 
 	
 	//Gets the media products that are below the given maxPrice
